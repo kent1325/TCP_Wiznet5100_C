55f6b71ec5914da9a49f32b79b719ed02f87f69a
Kent Vugs Nielsen <kent.vugs@hotmail.com>
2016-11-01 09:07:46 +0100

    Update nic_settings.h

--- SimpleTCP/SimpleTCP/nic_settings.h
+++ SimpleTCP/SimpleTCP/nic_settings.h
@@ -2,12 +2,12 @@
 #define NIC_SETTINGS_H_
 
 // Ethernet specifikke settings
-unsigned char mac_addr[] = {0x90,0xA2,0xDA,0x0D,0xD7,0xD5};
-unsigned char ip_addr[] = {192,168,0,2};
+unsigned char mac_addr[] = {0x90,0x1B,0x0E,0x67,0x6B,0x98};
+unsigned char ip_addr[] = {192,168,0,5};
 unsigned char sub_mask[] = {255,255,255,0};
 unsigned char gtw_addr[] = {192,168,0,1}; 
 
-// Max størrelse på vores RX/TX Buffer her i microen.
+// Max stÃ¸rrelse pÃ¥ vores RX/TX Buffer her i microen.
 #define MAX_BUF 25
 
 #endif /* NIC_SETTINGS_H_ */
